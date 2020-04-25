2532b1450a42aa69699180d8f68ce9f9024f4d60
AsadKamran92 <asad.kamran45@outlook.com>
2020-04-26 02:42:56 +0800

    Wuzhou-University-Store

---
+++ client/src/pages/contact/contact.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+    return(
+    <div>
+        <h1> Name :  Asad Kamran </h1>
+        <h2> Email : asadlimagnifique@gmail.com</h2>
+        <h2> Contact No : 18587477425 </h2>
+        <h2> Id No : 201600201002 </h2>
+    </div>
+    )
+} 
+
+export default Contact;
